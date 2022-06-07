abad61aadd45f7ea5088db96bd7c7049fac5823e
Abimanyu <hello@abimanyu.co>
2022-06-07 12:23:54 +0700

    Fix handshake inactivity timeout

--- connection/dbConnect.js
+++ connection/dbConnect.js
@@ -4,9 +4,13 @@ const mysql = require('mysql');
 const dbConfig = {
     connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     multipleStatements: true
 }
 
